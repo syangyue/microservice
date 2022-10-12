a9e04a606ca7cd81a057304fcbf7fffe8076edf8
yangyue <daisysong19900215@gmail.com>
2022-10-12 09:17:54 -0700

    Add location names

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,14 +1,18 @@
-function createCard(title, description, pictureUrl) {
+function createCard(title, location, description, pictureUrl, starts, ends) {
     return `
-        <div class="card">
-          <div class="card mb-3 shadow">
-            <img src="${pictureUrl}" class="card-img-top">
-            <div class="card-body">
+    <div>
+       <div class="shadow p-3 mb-5 bg-body rounded">
+         <img src="${pictureUrl}" class="card-img-top" alt="elephant">
+           <div class="card-body">
              <h5 class="card-title">${title}</h5>
+             <h6 class="card-subtitle mb-2 text-muted">${location}</h6>
              <p class="card-text">${description}</p>
-            </div>
+           </div>
+           <div class="card-footer">
+             ${starts} - ${ends}
            </div>
         </div>
+    </div>
     `;
   }
 
@@ -22,19 +26,20 @@ window.addEventListener("DOMContentLoaded", async () => {
       console.log("got an error");
     } else {
       const data = await response.json();
-      let index = 0;
       for (let conference of data.conferences) {
         const detailUrl = `http://localhost:8000/${conference.href}`;
         const detailResponse = await fetch(detailUrl);
         if (detailResponse.ok) {
           const details = await detailResponse.json();
           const title = details.conference.name;
+          const location = details.conference.location.name;
           const description = details.conference.description;
           const pictureUrl = details.conference.location.picture_url;
-          const html = createCard(title, description, pictureUrl);
-          const column = document.querySelector(`#col-${index % 3}`);
+          const starts = new Date(details.conference.starts).toLocaleDateString();
+          const ends = new Date(details.conference.ends).toLocaleDateString();
+          const html = createCard(title, location, description, pictureUrl, starts, ends);
+          const column = document.querySelector(`.row`);
           column.innerHTML += html;
-          index += 1;
         }
       }
     }
