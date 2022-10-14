ac9f6f1832e51de8cfafe1b6458b07db991324c2
yangyue <daisysong19900215@gmail.com>
2022-10-13 21:51:19 -0700

    create a new conference

---
+++ ghi/js/new-conference.js
@@ -0,0 +1,37 @@
+window.addEventListener("DOMContentLoaded", async () => {
+    const url = "http://localhost:8000/api/locations/";
+    const response = await fetch(url);
+    if (response.ok) {
+        const selectTag = document.getElementById('location');
+        const data = await response.json();
+        console.log(data)
+        for (let location of data.locations) {
+            let option = document.createElement('option');
+            option.value = location.id;
+            option.innerHTML = location.name;
+            selectTag.appendChild(option);
+
+        }
+    }
+    const formTag = document.getElementById('create-conference-form');
+    formTag.addEventListener('submit', async event => {
+        event.preventDefault();
+        const formData = new FormData(formTag);
+        const json = JSON.stringify(Object.fromEntries(formData));
+        const conferenceUrl = 'http://localhost:8000/api/conferences/';
+        const fetchOptions = {
+            method: "POST",
+            body: json,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+        const newResponse = await fetch(conferenceUrl, fetchOptions);
+        if (newResponse.ok) {
+            formTag.reset();
+            const newConference = await newResponse.json();
+            console.log(newConference);
+        }
+
+    });
+});
